abdbff2580dd21163ed2d2da3a532647e9f01fd1
AndreyLay <Andrey.Lay@nw.rt.ru>
2019-04-12 10:22:59 +0300

    исправленный

--- My_module1.py
+++ My_module1.py
@@ -3,17 +3,17 @@ import sys, os
 
 name = "Dir"
 
-def Mk_dir():
+def mk_dir():
 
     for i in range(1, 10):
         new_path = os.path.join(os.getcwd(), '{}-{}'.format(name, i))
         #print(os.getcwd())
         os.mkdir(new_path)
 
-def Del_dir():
+def del_dir():
     for i in range(1, 10):
         new_path = os.path.join(os.getcwd(), '{}-{}'.format(name, i))
         os.rmdir(new_path)
 
-#Mk_dir()
-#Del_dir()
+#mk_dir()
+#del_dir()
