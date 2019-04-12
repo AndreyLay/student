f404eecb668c3a0af4bc14b76329466d2e95fde9
AndreyLay <Andrey.Lay@nw.rt.ru>
2019-04-12 10:22:39 +0300

    Исправленный

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 
+from Lesson5.my_module1 import mk_dir, del_dir
+from Lesson5.my_module2 import rand_elem
 
-from My_module1 import Mk_dir, Del_dir
-from My_module2 import Rand_elem
+Choice = int(input('{Хотите создать директории - нажмите "1", хотите удалить - "2", хотите случайный элемент - "3": '))
+if Choice == 1:
+    mk_dir()
+elif Choice == 2:
+    del_dir()
+elif Choice == 3:
+    rand_elem()
+else:
+    print('Вы ввели неправильное значение')
 
-#Mk_dir
-#Del_dir
-Rand_elem
+
