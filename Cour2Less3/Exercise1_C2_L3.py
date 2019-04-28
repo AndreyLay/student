b274df436d2584c36a941f8d1a39feaa6acc6281
AndreyLay <Andrey.Lay@nw.rt.ru>
2019-04-28 14:23:09 +0300

    IATA код по названию города

---
+++ Cour2Less3/Exercise1_C2_L3.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+origin = input("Введите название города: ")
+link = "https://www.travelpayouts.com/widgets_suggest_params?q=Из%20"+origin+"%20в%20Лондон"
+try:
+    data = json.loads(requests.get(link).text)
+    data = data['origin']
+    print('IATA код запрошенного города:', data['iata'])
+except:
+    print("Нам жаль. Этому городу пока не присвоен код IATA.")
