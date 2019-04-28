4ea06d1bc0d4aa49f0107a61a79e8eb983f994ae
AndreyLay <Andrey.Lay@nw.rt.ru>
2019-04-28 14:46:26 +0300

    Исправленный с ф-цией

--- Cour2Less3/Exercise1_C2_L3.py
+++ Cour2Less3/Exercise1_C2_L3.py
@@ -1,11 +1,14 @@
 import requests
 import json
 
-origin = input("Введите название города: ")
-link = "https://www.travelpayouts.com/widgets_suggest_params?q=Из%20"+origin+"%20в%20Лондон"
-try:
-    data = json.loads(requests.get(link).text)
-    data = data['origin']
-    print('IATA код запрошенного города:', data['iata'])
-except:
-    print("Нам жаль. Этому городу пока не присвоен код IATA.")
+def iata_code():
+    origin = input("Введите название города: ")
+    link = "https://www.travelpayouts.com/widgets_suggest_params?q=Из%20"+origin+"%20в%20Лондон"
+    try:
+        data = json.loads(requests.get(link).text)
+        data = data['origin']
+        print('IATA код запрошенного города:', data['iata'])
+    except:
+        print("Нам жаль. Этому городу пока не присвоен код IATA.")
+
+iata_code()
